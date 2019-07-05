9be92f0dc21520738eab4ad4a33066ba4769e160
Juni Yadi <juniyadi94@gmail.com>
2019-07-05 10:43:37 +0700

    CM-21 fix(dbid): Fixed Fillable Content Metadata Model

--- app/Models/ContentMetadataModel.php
+++ app/Models/ContentMetadataModel.php
@@ -6,7 +6,27 @@ use BaoPham\DynamoDb\DynamoDbModel;
 
 class ContentMetadataModel extends DynamoDbModel
 {
-    protected $fillable = ['title', 'year'];
+    protected $fillable = [
+        'category_id',
+        'name_uploader',
+        'video_title',
+        'video_description',
+        'video_subtitle',
+        'video_genre',
+        'video_format',
+        'video_size',
+        'video_resolution',
+        'video_duration',
+        'video_viewers',
+        'video_comments',
+        'video_likes',
+        'video_dislikes',
+        'video_users_likes',
+        'video_users_dislikes',
+        'video_share',
+        'video_saves',
+        'video_downloads',
+    ];
 
     protected $table = 'ContentMetadata';
 }
