aeabf1b155e21d6decaeb5ccb39c6d82ad9d6a41
Juni Yadi <juniyadi94@gmail.com>
2019-08-10 13:25:59 +0700

    CM-10 feat(dbil): Added Controller ContentDislike

---
+++ app/Http/Controllers/ContentDislikeController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use App\Models\ContentDislikeModel;
+use App\Traits\DndbQuery;
+
+class ContentDislikeController extends Controller
+{
+    // Include Trait inside Controller
+    use DndbQuery;
+
+    /**
+     * @param ContentDislikeModel $model
+     * @param Request $request
+     */
+    public function index(ContentDislikeModel $model, Request $request)
+    {
+        $count = $model->count();
+
+        if($count == 0) {
+            $message    = ', no data found with this query';
+            $result     = [];
+        } else {
+            $message    = ', data has been found';
+            $result     = $model->all();
+        }
+
+        return response()->json([
+            'status' => [
+                'code'      => '200',
+                'message'   => 'index list query has been performed'. $message,
+                'total'     => $count,
+            ],
+            'result' => $result,
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'user_id'       => 'required',
+            'metadata_id'   => 'required'
+        ]);
+
+        $user_id            = $request->input('user_id');
+        $metadata_id        = $request->input('metadata_id');
+
+        $query              = new ContentDislikeModel();
+        $query->id          = Str::uuid()->toString();
+        $query->user_id     = $user_id;
+        $query->metadata_id = $metadata_id;
+        $query->save();
+
+        return response()->json([
+            'status' => [
+                'code'      => '200',
+                'message'   => 'data has been saved',
+            ],
+            'result' => [
+                'id' => $query->id,
+            ],
+        ], 200);
+    }
+
+    public function show(ContentDislikeModel $model, string $id)
+    {
+        return $model->findOrFail($id);
+    }
+
+    public function search(ContentDislikeModel $model, Request $request)
+    {
+        $this->validate($request, [
+            'query' => 'required',
+        ]);
+
+        $query = $request->input('query');
+
+        // Parse Function From Trait DndbQuery
+        $query_eloquent = $this->ReqParse($query);
+
+        // Performe Query
+        $query = $model->where($query_eloquent);
+        $count = $query->count();
+
+        if($count == 0) {
+            $message    = ', no data found with this query';
+            $result     = [];
+        } else {
+            $message    = ', data has been found';
+            $result     = $query->get();
+        }
+
+        return response()->json([
+            'status' => [
+                'code'      => '200',
+                'message'   => 'search query has been performed'. $message,
+                'total'     => $count,
+            ],
+            'result' => $result,
+        ], 200);
+    }
+
+    public function update(ContentDislikeModel $model, Request $request, string $id)
+    {
+        $query = $model->findOrFail($id);
+        $query->update($request->all());
+
+        return response()->json([
+            'status' => [
+                'code'      => '200',
+                'message'   => 'data has been updated',
+            ],
+            'result' => [
+                'id' => $query->id,
+            ],
+        ], 200);
+    }
+
+    public function delete(ContentDislikeModel $model, string $id)
+    {
+        $query = $model->findOrFail($id);
+        $query->delete();
+
+        return response()->json([
+            'status' => [
+                'code'      => '200',
+                'message'   => 'data has been deleted',
+            ],
+        ], 200);
+    }
+}
